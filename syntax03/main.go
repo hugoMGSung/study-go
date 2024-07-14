6cf69580dc00f5a105ab1fa0bb6ac4d1d8fcac7a
Hugo MG Sung <personar95@naver.com>
2024-07-14 21:55:43 +0900

    왜 이러지?

--- syntax03/main.go
+++ syntax03/main.go
@@ -30,4 +30,72 @@ func main() {
 	}
 
 	fmt.Println(arr4)
+
+	// 슬라이스 변수 선언
+	var origin []int
+	origin = []int{1, 2, 3, 4, 5} // 슬라이스에 리터럴값 지정
+	origin[4] = 15
+
+	fmt.Println(origin)
+
+	// make() 함수 사용
+	seconds := make([]int, 5, 15)           // 5(슬라이스 길이), 15(내부 배열 최대길이)
+	fmt.Println(len(seconds), cap(seconds)) // len() 길이, cap() Capacity
+
+	// 슬라이스에 별도의 길이와 용량을 지정하지 않으면, 0인 슬라이스 만듬
+	// Nil Slice라고 함
+	var slice []int
+
+	if slice == nil {
+		fmt.Println("Nil Slice")
+	}
+	fmt.Println(len(slice), cap(slice)) // 모두 0
+
+	// 부분 슬라이스
+	threes := []int{1, 2, 3, 4, 5}
+	var fours = threes[2:4]
+	fmt.Println(fours)
+
+	var fifth = threes[3:]
+	fmt.Println(fifth)
+
+	// append
+	threes = append(threes, 7, 9)
+	fmt.Println(threes)
+
+	// len=0, cap=3 인 슬라이스
+	sliceA := make([]int, 0, 3)
+
+	// 계속 한 요소씩 추가
+	for i := 1; i <= 15; i++ {
+		sliceA = append(sliceA, i)
+		// 슬라이스 길이와 용량 확인
+		fmt.Println(len(sliceA), cap(sliceA))
+	}
+
+	fmt.Println(sliceA)
+
+	// copy()
+	source := []int{1, 2, 3}
+	target := make([]int, len(source), cap(source)*3)
+	copy(target, source)
+	fmt.Println(target)
+	println(len(target), cap(target))
+
+	// Map
+	var intHt = map[int]string{}
+	intHt[100] = "FIRST"
+	intHt[200] = "SECOND"
+	intHt[300] = "THIRD"
+	fmt.Println(intHt[300])
+
+	var stringHt = make(map[string]string)
+	stringHt["ONE"] = "Hundred"
+	stringHt["TWO"] = "Thousand"
+	stringHt["THREE"] = "Million"
+	fmt.Println(stringHt["TWO"])
+
+	for key, value := range stringHt {
+		fmt.Println(key, value)
+	}
 }
